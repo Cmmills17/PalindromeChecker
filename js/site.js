b3511c3ca57c97d349ee330d046a66a6d529135c
Cmmills17 <Cameronmills0713@gmail.com>
2025-04-23 12:27:32 -0400

    Fixed Palindrome. Todo: Fix extra credit

--- js/site.js
+++ js/site.js
@@ -1,10 +1,8 @@
 // get message that was entered
 function getValues() {
-    let alertDiv = document.getElementById('alertDiv');
-    alertDiv.classList.add('invisible');
+
     //find the input id="msgInput" Element
     let inputElement = document.getElementById('msgInput');
-
     //get what the user typed out of the <input />
     let userString = inputElement.value;
 
@@ -12,40 +10,55 @@ function getValues() {
     // palindrome check the message
     let palString = checkForPalindrome(userString);
 
-
+    //send to display
     displayResults(palString);
-
-
 }
 
 //check the message
 function checkForPalindrome(message) {
-    message = message.replace(/[ \p{P}]/gu, '').toLowerCase();
+
+    normMessage = message.replace(/[ \p{P}]/gu, '').toLowerCase();
+
     let palWord = '';
 
-    for (let i = message.length - 1; i >= 0; i -= 1) {
+    for (let i = normMessage.length - 1; i >= 0; i -= 1) {
 
         let char = message[i];
 
         palWord = palWord + char;
     }
 
-    if (message == palWord) {
+    if (normMessage == palWord) {
         return true;
+
     } else {
         return false;
+    }
+}
+// display the message
+function displayResults(checkedMessage) {
+    // find alertDiv
+    let alertDiv = document.getElementById('alertDiv');
+    // remove "invisible" class
+    alertDiv.classList.remove('invisible');
 
+    // find alertHeading
+    let heading = document.getElementById('alertHeading');
+    // find alertmsg
+    let body = document.getElementById('alertMsg')
+
+    if (checkedMessage == true) {
+        // set alertHeading text to "success"
+        heading.innerText = 'Well Done!';
+        // set alertMsg text to "thats a palindrome"
+        body.innerHTML = 'That <b>is</b> a Palindrome!';
+    } else {
+        // set alertHeading text to "uh oh"
+        heading.innerText = 'Ohh No';
+        // set alertMsg text to 'thats not a palindrome'
+        body.innerHTML = 'That is <b>not</b> a Palindrome';
     }
 
 
-
-}
-
-function displayResults(checkedMessage) {
-    let paragraphTag = document.getElementById('alertMsg');
-    paragraphTag.innerHTML = `Your Palindrome is: <b>${checkedMessage}`;
-
-    let alertDiv = document.getElementById('alertDiv');
-    alertDiv.classList.remove('invisible');
 
 }
